1bc4398c29e43c8095b817c9aaf0721c25b0d1b0
ANSHU AYUSH KUMAR <anshuayushkumar2006@gmail.com>
2026-09-13 23:23:39 +0530

    Add files via upload

---
+++ spam.py
@@ -0,0 +1,10 @@
+# pyright: reportMissingModuleSource=false
+import pyautogui
+import time
+time.sleep(7)
+count = 0
+while count <= 50:
+	pyautogui.typewrite("SLAYY SUNNYY")
+	pyautogui.press("enter")
+	count = count + 1
+	time.sleep(3)
